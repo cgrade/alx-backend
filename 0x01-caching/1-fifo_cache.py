4823b66674941cfd0238f9cb30ae4fa504eb183a
cgrade <gradeisageek@gmail.com>
2023-08-28 02:10:57 +0100

    Fix: Corrections made

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,33 +4,42 @@ A module that contains  class that inherited from a base class
 `BaseCaching
 """
 
-from basic import base_caching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(base_caching.BaseCaching):
-    """ FIFO Class that inherits from a Base Class
+class FIFOCache(BaseCaching):
+    """_summary_
     """
+
     def __init__(self):
-        """ Initialization
+        """_summary_
         """
         super().__init__()
 
     def put(self, key, item):
-        """ A method that insert data into the cache
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is None or item is None:
             pass
         else:
-            if len(list(self.cache_data)) >= self.MAX_ITEMS and key not in \
-                   self.cache_data.keys():
-                first_item = list(self.cache_data)[0]
-                print(f'DISCARD: {first_item}')
-                self.cache_data.pop(first_item)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
+
             self.cache_data[key] = item
 
     def get(self, key):
-        """ A method that returns the given key
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
         """
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
