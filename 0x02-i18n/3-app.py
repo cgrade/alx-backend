9655b9287935fe1bb39199c8634a4cf3aa9f5e45
cgrade <gradeisageek@gmail.com>
2023-08-30 02:04:36 +0100

    soluion to task 3 added with all the .pot, .po and .mo files

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" A Basic Flask app that renders 0-index.html template
+"""
+
+
+from flask_babel import (
+    Babel,
+    gettext
+)
+import typing
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+
+class Config:
+    """ A class that configures available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Initialize a flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ A local selector to select the default language for user
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index() -> typing.Any:
+    """ A simple route in the Flask App
+    """
+    home_title = gettext('Welcome to Holberton')
+    home_header = gettext('Hello world!')
+    return render_template('3-index.html', home_title=home_title, home_header=home_header)
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
